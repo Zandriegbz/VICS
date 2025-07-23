fa6087416d5920ecd54620a266f3fe99a76d5572
darwey <taroroie@gmail.com>
2025-07-23 11:06:13 +0800

    ambot

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts (VICS)
 export const environment = {
   production: true,
-   apiUrl: 'http://172.16.12.103/TKT/api' // Development API URL
+   apiUrl: 'http://172.16.188.71/TKT/api' // Development API URL
 }; 
